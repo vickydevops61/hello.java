0284458e1edbab2eb416b8097b0f3b64ac4f4fc2
vickydevops61 <96574733+vickydevops61@users.noreply.github.com>
2021-12-23 17:40:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Happy new year");
-    System.out.println("happy birthday");
   }
 }
